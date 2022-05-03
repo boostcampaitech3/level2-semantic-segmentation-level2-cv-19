1ab5b3ffb9538caac17067ce9f6189daa81a0f95
Kwon <rnjsgurtkswk@gmail.com>
2022-05-03 23:36:06 +0000

    hr

--- mmsegmentation/_custom_configs_/hr_city_scape/fcn_hr18_512x512_40k_voc12aug.py
+++ mmsegmentation/_custom_configs_/hr_city_scape/fcn_hr18_512x512_40k_voc12aug.py
@@ -1,5 +1,7 @@
 _base_ = [
-    '../../configs/_base_/models/fcn_hr18.py', './dataset.py'
-    '../../configs/_base_/default_runtime.py', '../../configs/_base_/schedules/schedule_40k.py'
+    './fcn_hr18.py', './dataset.py',
+    
+    '/opt/ml/input/code/level2-semantic-segmentation-level2-cv-19/mmsegmentation/_custom_configs_/hr_city_scape/runtime.py',
+    '/opt/ml/input/code/level2-semantic-segmentation-level2-cv-19/mmsegmentation/_custom_configs_/hr_city_scape/schedule.py'
 ]
 model = dict(decode_head=dict(num_classes=21))
